6fdab0d591b2419223ef8e5ba004e831011b5290
RosaBen <benchabane.rosa@outlook.com>
2026-08-27 14:09:17 +0200

    added ternary for homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,17 +4,18 @@ import Ingredients from "../components/Ingredients";
 
 export default function Home({ ingredients }) {
   const [newIngr, setNewIngr] = useState(ingredients);
-  const style =
-    newIngr.length === 0 ? { display: "none" } : { display: "block" };
+  const styleList = newIngr.length ? { display: "block" } : { display: "none" };
+  const styleHome =
+    newIngr.length > 2 ? { display: "grid" } : { display: "none" };
 
   return (
     <main>
       <Form ingredients={ingredients} set={setNewIngr} />
-      <section className="list-ingredients" style={style}>
+      <section className="list-ingredients" style={styleList}>
         <h2>Ingredients on hand:</h2>
         <Ingredients newArray={newIngr} />
       </section>
-      <section className="get-recipe">
+      <section className="get-recipe" style={styleHome}>
         <h2>Ready for a recipe?</h2>
         <p>Generate a recipe from your list of ingredients.</p>
         <button>Get a recipe</button>
